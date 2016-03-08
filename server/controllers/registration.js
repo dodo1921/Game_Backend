78fc2f358ebc90e2c2db3da106f447f982b0bb87
Mayukh Chakraborty <mayukh@arnium.com>
2016-03-08 09:34:00 +0530

    update

--- server/controllers/registration.js
+++ server/controllers/registration.js
@@ -1,5 +1,6 @@
 
 var rdbms  = require('../../server/utils/query');
+var speakeasy = require('speakeasy');
 
 
 var Registrar = module.exports;
@@ -9,21 +10,65 @@ Registrar.registerPhoneNumber = function(req, res) {
 
 	var pno = parseInt(req.body.pno);
 
-	console.log('PNO::'+pno);
-
 	var querytext = 'SELECT "id", "isRegis", "createdTime" FROM "Users" where "pno"=($1)';
 
 	var values = [pno];
 
 	rdbms.query(querytext, values, function(err, rows, result){
 
-		if(err) console.log( 'Error::' + err);
+		if(err) res.json({'error':'Database Error'});
 
-		console.log( 'RESULT:::' + result );	
+		if(rows && rows.length>0){			
 
-		return res.json({'success': rows});
+			//create new vcode
+			//Update
 
-	})
+			var se = speakeasy.totp({key: 'secret'});
+			querytext = 'UPDATE "Users" SET vcode=($1) WHERE "id"=($2)';
+
+			values = [se, rows[0].id];
+
+			rdbms.query(querytext, values, function(err, rows, result){
+
+				if(err) res.json({'error':'Database Error'});
+
+				//send sms
+
+				return res.json({'status': 'success' });
+
+
+			});
+
+
+			
+
+		}else if(rows && rows.length == 0){
+
+			//create new vcode
+			//insert
+
+				var se = speakeasy.totp({key: 'secret'});
+				querytext = 'INSERT INTO "Users"( "pno", "vcode" ) VALUES ( ($1), ($2))';
+
+				values = [pno, se];
+
+				rdbms.query(querytext, values, function(err, rows, result){
+
+					if(err) res.json({'error':'Database Error'});
+
+					//send sms
+
+					return res.json({'status': 'success' });
+
+					
+				});
+
+		}	
+
+		
+	}
+
+});
 	
 
 	
